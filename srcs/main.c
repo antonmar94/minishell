4407809f9cb3d735990d0f2406898d81d6b365a8
alberto <albzamor@gmail.com>
2021-12-21 22:59:20 +0100

    header and .h

--- srcs/main.c
+++ srcs/main.c
@@ -1,23 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.c                                        :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: albzamor <albzamor@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/17 17:11:21 by antonmar          #+#    #+#             */
-/*   Updated: 2021/12/21 21:00:16 by albzamor         ###   ########.fr       */
+/*   Updated: 2021/12/21 22:58:19 by albzamor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+#include "../includes/minishell.h"
 
 int main()
 {
-	int i;
-
-	printf("MINISHELL\n");
-	i = 0;
+	header();
+	//printf("MINISHELL\n");
 	while(1)
+	{
+		//imprimir prompt con la ruta:
+		//readline() //capturar linea comandos
+		sleep(1);
+	}
 	return (0);
 }
