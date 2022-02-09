d9430d84c837f69587a17ed141e3e0041531696b
Alberto <fabricadenubes@gmail.com>
2022-02-09 16:35:37 +0100

    working next add space between

--- srcs/1d_search_dollar.c
+++ srcs/1d_search_dollar.c
@@ -6,12 +6,13 @@
 /*   By: albzamor <albzamor@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/07 20:28:58 by albzamor          #+#    #+#             */
-/*   Updated: 2022/02/08 16:27:38 by albzamor         ###   ########.fr       */
+/*   Updated: 2022/02/09 16:34:20 by albzamor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
+/* if found one return de var content */
 char *search_var_coincident(t_shell *shell, char* str_to_find)
 {
 
@@ -32,6 +33,7 @@ char *search_var_coincident(t_shell *shell, char* str_to_find)
 
 }
 
+/* modified split to return de first str before a char */
 char	*ft_split_one(char const *s, char c)
 
 {
@@ -60,37 +62,59 @@ int	change_dollars(t_shell *shell)
 	char *origin_line_arg;
 	char	*begin$;
 	char	*first_$_found;
-	int 	last_pos;
+	char	*line_until$_joined;
+	int 	last_pos_until$;
 	int		size_arg;
-	char *trozo;
+	int		count_until$;
+	char *line_until$;
+	char *new_expanded_str;
 	char *content;
 
 	origin_line_arg = shell->line;
-	last_pos = 0;
-	printf("\nhola\n");
-	printf("\nfake arguments: %s\n\n", shell->line);
+	last_pos_until$ = 0;
+	count_until$ = 0;
+	printf("fake arguments: ");
+	printf(RED"%s\n\n"RESET, shell->line);
 
 
 	while (shell->line&& *(shell->line))
 	{
 		if (*shell->line != '$')
+		{
 			shell->line++;
+			count_until$++;
+		}
+
 		else
 		{
 			shell->line++;
 			begin$ = shell->line;
 			first_$_found = ft_split_one(begin$, ' ');
-			size_arg = ft_strlen(first_$_found);
 			content = search_var_coincident(shell, first_$_found);
+			size_arg = ft_strlen(first_$_found);
+			printf("\ncontent: %s\n", content);
 			if (content)
 			{
-				trozo =ft_strjoin(ft_substr(shell->line, last_pos, last_pos + size_arg), content);
-				last_pos +=size_arg;
-				printf("trozo %s", trozo);
+
+				printf("count_until$%d", count_until$);
+				line_until$ = ft_substr(origin_line_arg, last_pos_until$, count_until$ -1);
+				if(new_expanded_str)
+				{
+					line_until$_joined = ft_strjoin(new_expanded_str, line_until$);
+					free(new_expanded_str);
+				}
+				else
+					line_until$_joined = line_until$;
+				printf("\nline_until$: %s\n", line_until$);
+				printf("\nfirst$_found; %s\n", first_$_found);
+				origin_line_arg += size_arg + ft_strlen(line_until$) -1;//var name + char $
+				new_expanded_str =ft_strjoin(line_until$_joined, content);
+				free(line_until$);
+				last_pos_until$+=size_arg;
+				count_until$ = 0;
 			}
-			printf("\nprimera$: %s\n", first_$_found);
-			//printf("\nsize primera %d:\n", size_arg);
 			shell->line+=size_arg;
+			printf("\nline_until$_joined: %s\n", new_expanded_str);
 		}
 
 	}
