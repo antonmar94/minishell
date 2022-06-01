b29cd717c825888709135a7fc403ffafc4405fa0
antonmar94 <2009ajmt@gmail.com>
2022-06-01 22:19:08 +0200

    redirecciones funcionan perfecto

--- srcs/10_redirect.c
+++ srcs/10_redirect.c
@@ -6,121 +6,11 @@
 /*   By: antonmar <antonmar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/21 20:05:39 by albzamor          #+#    #+#             */
-/*   Updated: 2022/05/31 22:44:38 by antonmar         ###   ########.fr       */
+/*   Updated: 2022/06/01 22:18:20 by antonmar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
-
-//FALTA LA MIERDA DE LAS COMILLAS AQUI TAMBIÉN
-
-//Crear una lista con todos los archivos que voy a crear,
- //cuyo último será en el que este el resultado de lo que se ejecuta en la linea.
-
-/* void	add_toline(char	**line, char **files)
-{
-	char	*no_arrow;
-	char	*aux;
-
-	no_arrow = *files;
-	no_arrow++;
-	while (*no_arrow && *no_arrow != '>') //buscar el proximo espacio que no tenga '>' despues
-	{
-		if (*no_arrow == ' ')
-		{
-			aux = no_arrow;
-			while (*aux == ' ' && *aux != '>')
-				aux++;
-			if (*aux && *aux != '>')
-			{
-				*no_arrow++ = '\0';
-
-			}
-			no_arrow++;
-		}
-		no_arrow++;
-	}	
-		
-	if (!*no_arrow)
-	{
-		no_arrow = *ptr;
-		while (*no_arrow && *no_arrow != ' ')
-			no_arrow++;
-		if (*no_arrow == ' ')
-		{
-			*no_arrow++ = '\0';
-			*line = ft_strjoin(*line, " ");
-			*line = ft_strjoin(*line, ft_substr(no_arrow,
-						0, ft_strlen(no_arrow)));
-		}
-	}
-}
-
-int	next_file(char **files, char **file_to_create)
-{
-	char	*ptr;
-	int		size_file;
-
-	ptr = *files;
-	size_file = 0;
-	while (*ptr && *ptr != '>')
-	{
-		ptr++;
-		size_file++;
-	}
-	*file_to_create = ft_substr(*files, 0, size_file);
-	if (!*ptr)
-		return (0);
-	open(*file_to_create, O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	*files = ptr;
-	return (1);
-} 
-
-void	find_files(char **line, char **redirect_file, int redirect_flag)
-{
-	char	*ptr;
-	char	*files;
-	char	*file_to_create;
-
-	ptr = *redirect_file;
-	files = *redirect_file;
-	file_to_create = NULL;
-	while (redirect_flag)
-	{		
-		while (*ptr && (*ptr == ' ' || *ptr == '>'))
-			ptr++;
-		files = ptr;
-		add_toline(line, &files);
-		redirect_flag = next_file(&files, &file_to_create);
-		ptr = files;
-	}
-	*redirect_file = file_to_create;
-}
-*/
-
-int	check_redirect(char **line, char **redirect_file)
-{
-	char	*ptr;
-
-	ptr = *line;
-	while (*ptr)
-	{
-		if (*ptr == '>')
-		{
-			*ptr++ = '\0';
-			*redirect_file = ptr;
-			if (*ptr == '>')
-			{
-				(*redirect_file)++;
-				return (2);
-			}
-			else
-				return (1);
-		}
-		ptr++;
-	}
-	return (0);
-}
 
 int	jump_quotes(char **line_to_ignore)
 {
@@ -143,76 +33,123 @@ int	jump_quotes(char **line_to_ignore)
 	return (size_quotes);
 }
 
-int	get_file_size (char **line, char **files, char **next_file)
+int	check_redirect(char **line, char **rest_of_line)
 {
-	char		*file_finder;
-	int			size_file;
-	char		*aux_append;
-	int			size_append;
-	
-	size_file = 0;
-	while (**files == '>' || **files == ' ')
-		(*files)++;
-	file_finder = *files;
-	while (*file_finder && *file_finder != '>')
+	char	*arrow_finder;
+	char	*aux_finder;
+	int		line_size;
+	int		num_arrows;
+
+	arrow_finder = *line;
+	line_size = 0;
+	num_arrows = 0;
+	while (*arrow_finder)
 	{
-		size_append = 0;
-		size_file += jump_quotes(&file_finder);
-		printf("file finder [%s]\n", file_finder);
-		if (*file_finder == ' ')
+		if (*arrow_finder == '>')
 		{
-			while (*file_finder == ' ' && *file_finder != '>')
-				file_finder++;
-			printf("file finder encontro espacio [%s]\n", file_finder);
-			if (!*file_finder || *file_finder != '>')
-			{
-				aux_append = file_finder;
-				while (*aux_append && *aux_append != '>')
-				{
-					size_append += jump_quotes(&aux_append);
-					aux_append++;
-				}
-				*line = ft_strjoin(*line, ft_substr(file_finder,
-							0, size_append));
-				*files = aux_append; 
-				file_finder = *files;
-			}
+			aux_finder = arrow_finder;
+			aux_finder++;
+			if (*aux_finder == '>')
+				num_arrows = 2;
 			else
-				file_finder--;
-		}
-		size_file++;
-		file_finder++;
-	}
-	*next_file = file_finder;
-	return (size_file);
-}
-
-int	get_files(char **line, char **files, int num_arrows)
-{
-	int			size_file;
-	char		*next_file;
-
-	next_file = *files;
-	printf("hola [%s]\n", next_file);
-	while (next_file)
-	{
-		size_file = get_file_size(line, files, &next_file);
-		printf("size file [%i]\n", size_file);
-		if (!*next_file)
-		{
-			while (**files && **files == '>')
-			{
-				(*files)++;
-				num_arrows++;
-			}
+				num_arrows = 1;
+			*rest_of_line = arrow_finder;
+			*line = ft_substr(*line, 0, line_size);
 			return (num_arrows);
 		}
-		open(ft_substr(*files, 0, size_file), O_WRONLY | O_CREAT | O_TRUNC, 0664);
-		*files = next_file;
+		line_size++;
+		arrow_finder++;
 	}
-	return (0);
+	return (num_arrows);
 }
 
+void	get_line_execute (char **line, char **rest_of_line)
+{
+	char		*line_finder;
+	char		*aux_append;
+	int			size_append;
+
+	while (**rest_of_line == ' ')
+		(*rest_of_line)++;
+	line_finder = *rest_of_line;
+	while (*line_finder && line_finder)
+	{
+		jump_quotes(&line_finder);
+		size_append = 0;
+		if (*line_finder && *line_finder == ' ')
+		{
+			if (*line_finder && *line_finder != '>')
+			{
+				aux_append = line_finder;
+				while (*aux_append && *aux_append != '>')
+				{
+					if (check_allquotes(aux_append))
+						size_append += jump_quotes(&aux_append);
+					else
+						size_append++;
+					aux_append++;
+				}
+				*line = ft_strjoin(*line, ft_substr(line_finder,
+							0, size_append));
+				line_finder = aux_append;
+			}
+		}
+		if(*line_finder && *line_finder == '>')
+		{
+			line_finder++;
+			while (*line_finder && *line_finder == ' ')
+				line_finder++;
+		}
+		else if (*line_finder)
+			line_finder++;
+	}
+}
+
+int		get_create_files(char **rest_of_line, int num_arrows)
+{
+	char	*files_finder;
+	char	*aux_finder;
+	int		file_size;
+	
+	files_finder = *rest_of_line;
+	while (*files_finder)
+	{
+		jump_quotes(&files_finder);
+		file_size = 0;
+		if (*files_finder && *files_finder == '>')
+		{
+			while (*files_finder && (*files_finder == '>' || *files_finder == ' '))
+			{
+				if (*files_finder == '>')
+					num_arrows++;
+				files_finder++;
+			}
+			aux_finder = files_finder;
+			while (*aux_finder && (*aux_finder != '>' && *aux_finder != ' '))
+			{
+				if (check_allquotes(aux_finder))
+					file_size += jump_quotes(&aux_finder);
+				else
+					file_size++;
+				aux_finder++;
+			}
+			while (*aux_finder && (*aux_finder != '>'))
+			{
+				jump_quotes(&aux_finder);
+				aux_finder++;
+			}
+			if (!*aux_finder)
+			{
+				*rest_of_line = ft_substr(files_finder, 0, file_size);
+				return (num_arrows);
+			}
+			open(ft_substr(files_finder, 0, file_size), O_WRONLY | O_CREAT | O_TRUNC, 0664);
+			num_arrows = 0;
+		}
+		files_finder++;
+	}
+	return (num_arrows);
+}
 
 void	do_redirect(t_shell *shell)
 {
@@ -222,7 +159,11 @@ void	do_redirect(t_shell *shell)
 
 	all_files = NULL;
 	num_arrows = check_redirect(&shell->line, &all_files);
-	num_arrows = get_files(&shell->line, &all_files, num_arrows);
+	if (num_arrows)
+	{
+		get_line_execute(&shell->line, &all_files);
+		num_arrows = get_create_files(&all_files, num_arrows);
+	}
 	if (num_arrows == 1)
 	{
 		fd = open(all_files, O_WRONLY | O_CREAT | O_TRUNC, 0664);
